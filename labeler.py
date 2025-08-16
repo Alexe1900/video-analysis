37b3c371da6ed20b5d6a9ad6ead5733e6479e187
Alexe1900 <alexejbodnarchuk@gmail.com>
2025-08-16 10:31:50 +0200

    add: image labeler for classification

--- labeler.py
+++ labeler.py
@@ -1,2 +1,42 @@
 import os
+import cv2
 
+isClass = open('./classified.txt', 'r')
+classified = dict()
+
+classifiedN = int(isClass.readline().strip())
+
+for _ in range(classifiedN):
+    classified[isClass.readline().strip()] = 1
+
+isClass.close()
+
+directory = './DATASETS/ownHanoi/images'
+
+results = open('./classes.txt', 'a')
+
+try:
+    for file in os.scandir(directory):
+        if file.is_file():
+            imgName = file.path.split('\\')[-1]
+            if imgName in classified:
+                continue
+            image = cv2.imread(file.path)
+            cv2.imshow(imgName, image)
+            cv2.waitKey()
+
+            result = input('enter classes:')
+            results.write(f'{imgName} {result}\n')
+            classified[imgName] = 1
+            
+            cv2.destroyAllWindows()
+except KeyboardInterrupt:
+    print('interrupted')
+    isClass = open('./classified.txt', 'w')
+    filelist = [str(len(classified))]
+    for key in classified.keys():
+        filelist.append(key)
+    isClass.write('\n'.join(filelist))
+    isClass.close()
+
+results.close()
